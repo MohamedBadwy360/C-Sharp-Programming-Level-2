c1078cbd991a6651c12954126c0adf0d5174a8e0
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-29 20:11:03 +0200

    Multicast Delegate

---
+++ 05. Multicast Delegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05.Multicast_Delegate
+{
+    internal class Program
+    {
+        public delegate void MyDelegate(string message);
+        public static void Method1(string message)
+        {
+            Console.WriteLine($"Method 1 + {message}");
+        }
+        public static void Method2(string message)
+        {
+            Console.WriteLine($"Method 2 + {message}");
+        }
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = Method1;
+            myDelegate += Method2;
+
+            myDelegate("Hello");
+
+            myDelegate -= Method1;
+            myDelegate("Another message");
+        }
+    }
+}
