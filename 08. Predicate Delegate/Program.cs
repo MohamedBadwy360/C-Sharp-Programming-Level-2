511bc87153626544ef84492b1befde4085e5f1e1
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-29 20:38:49 +0200

    Predicate Delegate

---
+++ 08. Predicate Delegate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _08.Predicate_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result = IsEvenPredicate(5);
+
+            Console.WriteLine($"Is 5 even ? {result}");
+        }
+            
+        public static Predicate<int> IsEvenPredicate = IsEven;
+            
+        public static bool IsEven(int x)
+        {
+            return (x % 2 == 0);
+        }
+    }
+}
