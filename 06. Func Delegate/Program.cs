42865e1c77b248febf7ea29582effc1b804be576
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-29 20:21:15 +0200

    Func Delegate

---
+++ 06. Func Delegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06.Func_Delegate
+{
+    internal class Program
+    {
+        //delegate int SquareDelgate(int x);
+        public static Func<int, int> square = SquareMethod;
+
+        public static int SquareMethod(int x)
+        {
+            return x * x;
+        }
+        static void Main(string[] args)
+        {
+            //SquareDelgate square = new SquareDelgate(SquareMethod);
+
+            int result = square(5);
+            Console.WriteLine($"Result = {result}");
+        }
+    }
+}
