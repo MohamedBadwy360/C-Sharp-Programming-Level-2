d01c0be83e60bd60b6364c159be537b9b8bde30b
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-30 00:24:23 +0200

    C# using to create an alias

---
+++ 10. C# using to create an alias/Program.cs
@@ -0,0 +1,12 @@
+﻿using Print = System.Console;
+
+namespace _10.C__using_to_create_an_alias
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Print.WriteLine("C# using to Create Alias");
+        }
+    }
+}
