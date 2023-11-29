a480b3fcd013f66911e5a741088e10ee431d0268
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-30 00:29:45 +0200

    C# using static directive

---
+++ 11. C# using static directive/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using static System.Math;
+
+namespace _11.C__using_static_directive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = Sqrt(8);
+            Console.WriteLine($"X = {x}");
+        }
+    }
+}
