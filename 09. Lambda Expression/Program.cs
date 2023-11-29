1d6a7bbb7036cd598eddfb6bcfefb10c3f6f0214
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-30 00:11:49 +0200

    Lambda Expression

---
+++ 09. Lambda Expression/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace _09.Lambda_Expression
+{
+
+    internal class Program
+    {
+        public static Func<int, int> square = (x) => x * x;
+
+        public static Action parameterlessAction = () =>
+            {
+                Console.WriteLine("This is parameterless Action.\n");
+            };
+
+        public static Action<int> intParameterAction = (x) =>
+            {
+                Console.WriteLine($"Action with int parameter {x}\n");
+            };
+
+        public static Action<int, string> intStringPrameterAction = (x, y) =>
+            {
+                Console.WriteLine($"Action with int parameter {x} and string parameter {y}\n");
+            };
+
+        public delegate int Operation(int x, int y);
+
+        public static void ExecuteOperation(int x, int y, Operation operation) 
+        {
+            int result = operation(x, y);
+            Console.WriteLine($"Result = {result}");
+        }
+        static void Main(string[] args)
+        {
+            int result = square(5);
+            Console.WriteLine($"Result = {result}\n");
+
+            parameterlessAction();
+            intParameterAction(5);
+            intStringPrameterAction(5, "hello");
+
+            ExecuteOperation(10, 20, (x, y) => x + y);
+            ExecuteOperation(10, 20, (x, y) => x - y);
+        }
+    }
+}
