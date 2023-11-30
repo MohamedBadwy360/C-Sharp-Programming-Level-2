4ef16e642f22f29850440b897358dcc3eaf35dd7
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-30 18:25:40 +0200

    Obsolete Attribute

---
+++ 19. Obsolete Attribute/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace _19.Obsolete_Attribute
+{
+    public class MyClass
+    {
+        [Obsolete("This method will be deprecated in the future")]
+        public void method1()
+        {
+
+        }
+        public void method2() { }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            myClass.method1();
+
+            myClass.method2();
+        }
+    }
+}
