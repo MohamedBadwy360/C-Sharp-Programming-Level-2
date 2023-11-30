272860750229ca08d054a006aeb49560f9164793
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-30 23:17:10 +0200

    Generic Methods

---
+++ 24. Generic Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace _24.Generic_Methods
+{
+    public class Utility
+    {
+        public static void Swap<T> (ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Usage with integers
+            int a = 5, b = 6;
+            Console.WriteLine($"Before Swap: a = {a} & b = {b}");
+            Utility.Swap(ref a, ref b);
+            Console.WriteLine($"After Swap: a = {a} & b = {b}");
+
+            // Usage with strings
+            string x = "Mohamed", y = "ELsaid";
+            Console.WriteLine($"Before Swap: x = {x} & y = {y}");
+            Utility.Swap(ref x, ref y);
+            Console.WriteLine($"After Swap: x = {x} & y = {y}");
+        }
+    }
+}
---
+++ 25. Generic Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace _25.Generic_Classes
+{
+    public class GenericBox<T>
+    {
+        private T _content;
+
+        public GenericBox(T content)
+        {
+            _content = content;
+        }
+
+        public T BoxContent() => _content;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericBox<int> intBox = new GenericBox<int>(23);
+            Console.WriteLine($"Content of intContent = {intBox.BoxContent()}");
+
+            GenericBox<string> stringBox = new GenericBox<string>("Mohamed");
+            Console.WriteLine($"Content of stringBox = {stringBox.BoxContent()}");
+
+        }
+    }
+}
