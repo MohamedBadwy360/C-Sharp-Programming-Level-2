e2a3a1786bcd501088497dd8496937aad1a17ed1
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-30 20:04:26 +0200

    'Type' Class

---
+++ 20. 'Type' Class/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace _20._Type__Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(string);
+
+            Console.WriteLine("Type Information");
+            Console.WriteLine($"Name : {type.Name}");
+            Console.WriteLine($"Full Name : {type.FullName}");
+            Console.WriteLine($"Is Class? : {type.IsClass}");
+        }
+    }
+}
