7a57289a1b65581638001da69b9f191aa937df76
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-11-30 17:43:18 +0200

    Serialization Attributes

---
+++ 17. Serialization Attributes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace _17.Serialization_Attributes
+{
+    internal class Program
+    {
+        [Serializable]
+        class MyClass
+        {
+            // Will be Serialized
+            public int SerializedField;
+
+            // Will not be serialized
+            [NonSerialized]
+            public int NonSerializedField;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
