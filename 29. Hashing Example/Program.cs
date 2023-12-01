3d1c565ea395c2b267ccb0aa01712fc0f72e7b66
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-12-01 16:26:53 +0200

    Hashing Example

---
+++ 29. Hashing Example/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace _29.Hashing_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input data
+            string data = "Mohamed Badwy";
+
+            // Compute the SHA-256 hash of the input data
+            string hashedData = ComputeHash(data);
+
+            // Display the original data and its hash
+            Console.WriteLine(data);
+            Console.WriteLine(hashedData);
+
+            Console.WriteLine($"Hashed Data length : {hashedData.Length}");
+        }
+
+        static string ComputeHash(string input)
+        {
+            //SHA is Secutred Hash Algorithm.
+            // Create an instance of the SHA-256 algorithm
+            using(SHA256 sha256 = SHA256.Create())
+            {
+                // Compute the hash value from the UTF-8 encoded input string
+                byte[] hashbytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                return BitConverter.ToString(hashbytes).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
