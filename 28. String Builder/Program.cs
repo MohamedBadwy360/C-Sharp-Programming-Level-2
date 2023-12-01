b2b7eab4ceb9eb72c64ad82bcdf30c4c44cc0724
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-12-01 02:08:15 +0200

    String Builder

---
+++ 28. String Builder/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace _28.String_Builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int iterations = 200000;
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            ConcatenationString(iterations);
+            stopwatch.Stop();
+            Console.WriteLine($"String Concatenation using + took {stopwatch.ElapsedMilliseconds}");
+
+            Stopwatch stopwatch2 = Stopwatch.StartNew();
+            ConcatenationStringBuilder(iterations);
+            stopwatch2.Stop();
+            Console.WriteLine($"String Concatenation using StringBuilder took {stopwatch2.ElapsedMilliseconds}");
+        }
+        
+        static void ConcatenationString(int iterations)
+        {
+            string result = "";
+            for (int i = 0; i < iterations; i++)
+            {
+                result += "a";
+            }
+        }
+
+        static void ConcatenationStringBuilder(int iterations)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                sb.Append("a");
+            }
+
+            string result = sb.ToString();
+        }
+    }
+}
