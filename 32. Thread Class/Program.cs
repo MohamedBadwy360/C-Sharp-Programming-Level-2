fb325461f78b235768806648da268cacdefd25fb
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-12-04 18:14:57 +0200

    Thread Class

---
+++ 32. Thread Class/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace _32.Thread_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(Method1);
+            t.Start();
+            t.Join();
+            //Method1();
+
+            for (byte i = 0; i <= 20; i++)
+            {
+                Console.WriteLine($"Main => {i}");
+                Thread.Sleep(500);
+            }
+        }
+
+        static void Method1()
+        {
+            for (byte i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"Method 1 => {i}");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
