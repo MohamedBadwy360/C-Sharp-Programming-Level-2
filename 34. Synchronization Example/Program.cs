54b18c06971733992eef1f4c979e7c9b28e717de
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-12-08 01:53:14 +0200

    Synchronization Example

---
+++ 34. Synchronization Example/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace _34.Synchronization_Example
+{
+    internal class Program
+    {
+        static int sharedCounter = 0;
+        static object sharedLock = new object();
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(Increment);
+            Thread t2 = new Thread(Increment);
+
+            t1.Start();
+            t2.Start();
+
+            // Wait for both threads to complete
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine($"Final Counter Value: {sharedCounter}");
+            Console.ReadKey();
+        }
+
+        static void Increment()
+        {
+            lock (sharedLock)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    sharedCounter++;
+                }
+            }
+        }
+    }
+}
