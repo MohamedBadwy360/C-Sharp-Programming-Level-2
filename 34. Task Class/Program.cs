71c2a7c40b68d6000c45cbabe3bcafbc5cb51bdf
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-12-13 07:50:33 +0200

    Task Class

---
+++ 34. Task Class/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _34._Task_Class
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Create and run an asynchronous task
+            Task<int> resultTask = PerformAsyncOperation();
+
+            // Do some other work while waiting for the task to complete
+            Console.WriteLine("Doing some other work...");
+
+            // Wait for the task to complete and retrieve the result
+            int result = await resultTask;
+
+            // Process the result
+            Console.WriteLine($"Result: {result}");
+        }
+
+        static async Task<int> PerformAsyncOperation()
+        {
+            // Simulate an asynchronous operation
+            await Task.Delay(4000);
+
+            // Return a result
+            return 42;
+        }
+    }
+}
