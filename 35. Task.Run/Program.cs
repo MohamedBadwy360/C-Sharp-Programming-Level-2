9f4c33502159df8237284f8f176130b769b8be7f
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2023-12-13 08:12:56 +0200

    Task.Run

---
+++ 35. Task.Run/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace _35._Task.Run
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Task task1 = Task.Run(() => DownloadFile("Download File 1"));
+            Task task2 = Task.Run(() => DownloadFile("Download File 2"));
+
+            await Task.WhenAll(task1, task2);
+
+            Console.WriteLine("Task1 and Task2 completed.");
+        }
+
+        static void DownloadFile(string TaskName)
+        {
+            Console.WriteLine($"{TaskName} started.");
+            Thread.Sleep(1000);
+            Console.WriteLine($"{TaskName} completed.");
+        }
+    }
+}
